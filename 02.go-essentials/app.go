ff8d8ee6a2fa1b138b27401d4d10755e57efc1b5
Amaré Ngobeni <n.ngobeni@icloud.com>
2025-09-10 14:49:52 +0200

    type overide

--- 02.go-essentials/app.go
+++ 02.go-essentials/app.go
@@ -6,11 +6,11 @@ import (
 )
 
 func main() {
-	var investmentAmount = 1000
-	var expectedARR = 10
-	var investmentTimeFrame = 10
+	var investmentAmount float64 = 1000
+	var expectedARR float64 = 10
+	var investmentTimeFrame float64 = 10
 
-	var fv = float64(investmentAmount) * math.Pow(1+float64(expectedARR)/100, float64(investmentTimeFrame))
+	var fv = (investmentAmount) * math.Pow(1+(expectedARR)/100, (investmentTimeFrame))
 
 	fmt.Print(fv)
 }
