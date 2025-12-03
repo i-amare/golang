e21b7d746c927501ebf40fbfae0973ec21e9a04e
Amaré Ngobeni <n.ngobeni@icloud.com>
2025-12-03 16:06:53 +0200

    created rudementory profit calculator

---
+++ 02.go-essentials/profit-calc/app.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var revenue float64 = inputFloat64("Please input revenue: ")
+	var expenses float64 = inputFloat64("Please input expenses: ")
+	var taxRate float64 = inputFloat64("Please input tax rate: ")
+
+	preTaxProfit := revenue - expenses
+	taxExpense := taxRate * preTaxProfit
+	postTaxProfit := preTaxProfit - taxExpense
+	ratio := postTaxProfit / preTaxProfit
+
+	fmt.Println("Earnings before tax: ", preTaxProfit)
+	fmt.Println("Tax Expense: (", taxExpense, ")")
+	fmt.Println("Profit for period: ", postTaxProfit)
+	fmt.Println("Ratio: ", ratio)
+}
+
+func inputFloat64(msg string) float64 {
+	var value float64
+
+	fmt.Print(msg)
+	fmt.Scanln(&value)
+
+	return value
+}
---
+++ 02.go-essentials/profit-calc/go.mod
@@ -0,0 +1,3 @@
+module teamcodeberry.com/profit-calc
+
+go 1.25.0
