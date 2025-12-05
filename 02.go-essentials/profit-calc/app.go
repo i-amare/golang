ab84151fc5ca897e5b48e45a0854f42af04e4bfa
Amaré Ngobeni <n.ngobeni@icloud.com>
2025-12-05 17:36:54 +0200

    add func

--- 02.go-essentials/profit-calc/app.go
+++ 02.go-essentials/profit-calc/app.go
@@ -7,10 +7,7 @@ func main() {
 	var expenses float64 = inputFloat64("Please input expenses: ")
 	var taxRate float64 = inputFloat64("Please input tax rate: ")
 
-	preTaxProfit := revenue - expenses
-	taxExpense := taxRate * preTaxProfit
-	postTaxProfit := preTaxProfit - taxExpense
-	ratio := postTaxProfit / preTaxProfit
+	preTaxProfit, taxExpense, postTaxProfit, ratio := calcFinancials(revenue, expenses, taxRate);
 
 	fmt.Println("Earnings before tax: ", preTaxProfit)
 	fmt.Printf("Tax Expense: (%v)\n", taxExpense)
@@ -20,9 +17,16 @@ func main() {
 
 func inputFloat64(msg string) float64 {
 	var value float64
-
 	fmt.Print(msg)
 	fmt.Scanln(&value)
-
 	return value
+}
+
+func calcFinancials(revenue float64, expenses float64, taxRate float64) (float64, float64, float64, float64) {
+	preTaxProfit := revenue - expenses
+	taxExpense := taxRate * preTaxProfit
+	postTaxProfit := preTaxProfit - taxExpense
+	ratio := postTaxProfit / preTaxProfit
+
+	return postTaxProfit, taxExpense, postTaxProfit, ratio; 
 }
