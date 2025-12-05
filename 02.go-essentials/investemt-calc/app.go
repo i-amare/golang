41ca234f5ddfc18e198d72e3c37317af4d71076a
Amaré Ngobeni <n.ngobeni@icloud.com>
2025-12-05 17:21:26 +0200

    function with multiple return values

--- 02.go-essentials/investemt-calc/app.go
+++ 02.go-essentials/investemt-calc/app.go
@@ -11,11 +11,11 @@ func main() {
 	years := 10.0
 	expectedInflationRate := 2.5
 
+	fv, realFV := calcFV(investmentAmount, expectedARR, expectedInflationRate, years);
+	
 	fmt.Print("Please enter Investment Amount: ")
 	fmt.Scanln(&investmentAmount)
 	
-	fv := (investmentAmount) * math.Pow(1+(expectedARR)/100, (years))
-	realFV := fv / math.Pow(1+expectedInflationRate/100, years)
 
 	fmt.Println("Future Value: ", round(fv, 2))
 	fmt.Println("Real Future Value: ", round(realFV, 2))
@@ -24,4 +24,11 @@ func main() {
 func round(num float64, decimalPlaces int) float64 {
 	x := math.Pow(10, float64(decimalPlaces))
 	return math.Round(num*x) / x
+}
+
+func calcFV(amt float64, arr float64, inflationRate float64, period float64) (float64, float64) {
+	fv := (amt) * math.Pow(1+(arr)/100, (period))
+	realFV := fv / math.Pow(1+inflationRate/100, period)
+
+	return fv, realFV;
 }
