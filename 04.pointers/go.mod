ab09c1acb3168d36cbd2b6459210016b4abb347e
Amaré Ngobeni <n.ngobeni@icloud.com>
2026-01-03 14:30:31 +0200

    start pointer module

---
+++ 04.pointers/go.mod
@@ -0,0 +1,3 @@
+module teamcodeberry.com/pointer
+
+go 1.25.0
---
+++ 04.pointers/pointer.go
@@ -0,0 +1,9 @@
+package main
+
+func main() {
+	age := 32
+	agePointer := &age
+
+	println("Age Pointer: ", age)
+	println("Age Pointer: ", agePointer)
+}
