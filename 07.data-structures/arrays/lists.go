ce302bd0a0c8bc3a0291f483da347217aeb2d455
Amaré Ngobeni <n.ngobeni@icloud.com>
2026-01-09 21:35:50 +0200

    slices

--- 07.data-structures/arrays/lists.go
+++ 07.data-structures/arrays/lists.go
@@ -9,8 +9,14 @@ func main() {
 	prices := [4]float32{9.9, 8, 3, 6.87}
 
 	productNames[1] = "Nook"
+	featuredPrices := prices[1:3]
+	highlightedPrices := featuredPrices[1:]
 
 	fmt.Println(prices)
 	fmt.Println(productNames)
+
 	fmt.Println(prices[2])
+
+	fmt.Println(featuredPrices)
+	fmt.Println(highlightedPrices)
 }
