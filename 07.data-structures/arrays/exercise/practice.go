a70ec7041c075928eae15dd9db266ecaca1a3f8d
Amaré Ngobeni <n.ngobeni@icloud.com>
2026-01-10 22:31:17 +0200

    output formatting

--- 07.data-structures/arrays/exercise/practice.go
+++ 07.data-structures/arrays/exercise/practice.go
@@ -6,17 +6,20 @@ func main() {
 	// 1
 	hobbyList := [3]string{"Running", "Climbing", "Hiking"}
 	fmt.Println(hobbyList)
-
+	fmt.Println("--------------------------------------------")
+	
 	// 2
 	hobbyListSlice := hobbyList[1:]
 	fmt.Println(hobbyList[0])
 	fmt.Println(hobbyListSlice)
-
+	fmt.Println("--------------------------------------------")
+	
 	// 3
 	slice1 := hobbyList[0:2]
 	slice2 := hobbyList[:2]
 	fmt.Println(slice1)
 	fmt.Println(slice2)
+	fmt.Println("--------------------------------------------")
 }
 
 // Time to practice what you learned!
