05e207a5065ad35d0bf9dad77ff1a6b4cae7ba8b
Amaré Ngobeni <n.ngobeni@icloud.com>
2026-01-10 22:01:40 +0200

    dynaimc array

--- 07.data-structures/arrays/lists.go
+++ 07.data-structures/arrays/lists.go
@@ -5,25 +5,29 @@ import (
 )
 
 func main() {
-	var productNames [4]string
-	prices := [4]float32{9.9, 8, 3, 6.87}
-
-	productNames[1] = "Nook"
-	featuredPrices := prices[1:3]
-	highlightedPrices := featuredPrices[1:]
-
+	prices := []float64{}
 	fmt.Println(prices)
-	fmt.Println(productNames)
-	fmt.Println(prices[2])
-	fmt.Println(featuredPrices)
-	fmt.Println(highlightedPrices)
-
-	highlightedPrices[0] = 219.78
-	fmt.Println(prices)
-
-	var bigArr [5]string
-	bigArr[0] = "a"
-	smallArr := bigArr[:2]
-	fmt.Println(len(bigArr), cap(bigArr))
-	fmt.Println(len(smallArr), cap(smallArr))
 }
+
+// func main() {
+// 	var productNames [4]string
+// 	prices := [4]float32{9.9, 8, 3, 6.87}
+
+// 	productNames[1] = "Nook"
+// 	featuredPrices := prices[1:3]
+// 	highlightedPrices := featuredPrices[1:]
+
+// 	fmt.Println(prices)
+// 	fmt.Println(productNames)
+// 	fmt.Println(prices[2])
+// 	fmt.Println(featuredPrices)
+// 	fmt.Println(highlightedPrices)
+
+// 	highlightedPrices[0] = 219.78
+// 	fmt.Println(prices)
+
+// 	var bigArr [5]string
+// 	smallArr := bigArr[:2]
+// 	fmt.Println(len(bigArr), cap(bigArr))
+// 	fmt.Println(len(smallArr), cap(smallArr))
+// }
