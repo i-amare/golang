ace737aa5127c95f44ec4fd094c1519fda010b2c
Amaré Ngobeni <n.ngobeni@icloud.com>
2026-01-10 22:30:13 +0200

    part 3

--- 07.data-structures/arrays/exercise/practice.go
+++ 07.data-structures/arrays/exercise/practice.go
@@ -11,6 +11,12 @@ func main() {
 	hobbyListSlice := hobbyList[1:]
 	fmt.Println(hobbyList[0])
 	fmt.Println(hobbyListSlice)
+
+	// 3
+	slice1 := hobbyList[0:2]
+	slice2 := hobbyList[:2]
+	fmt.Println(slice1)
+	fmt.Println(slice2)
 }
 
 // Time to practice what you learned!
