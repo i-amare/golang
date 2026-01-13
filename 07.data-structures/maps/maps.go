bcdf790195379b4899a588fb887469543b2aa9ff
Amaré Ngobeni <n.ngobeni@icloud.com>
2026-01-13 13:46:34 +0200

    testing make function

--- 07.data-structures/maps/maps.go
+++ 07.data-structures/maps/maps.go
@@ -5,13 +5,21 @@ import (
 )
 
 func main() {
-	websites := map[string]string{
-		"Google":              "https://google.com",
-		"Amazon Web Services": "https://aws.com",
-	}
-	fmt.Println(websites)
+	// websites := map[string]string{
+	// 	"Google":              "https://google.com",
+	// 	"Amazon Web Services": "https://aws.com",
+	// }
+	// fmt.Println(websites)
 	
-	websites["Air BnB"] = "https//:airbnb.com"
-	delete(websites, "Google")
-	fmt.Println(websites)
+	// websites["Air BnB"] = "https//:airbnb.com"
+	// delete(websites, "Google")	
+	// fmt.Println(websites)
+
+	arr := make([]int, 0, 10)
+
+	arr = append(arr, 1)
+	arr = append(arr, 2)
+
+	fmt.Println("Array Values: ", arr)
+	fmt.Println("Array Length: ", len(arr), "Array Cap: ", cap(arr))
 }
