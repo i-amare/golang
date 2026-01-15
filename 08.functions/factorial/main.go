84fb4504a9b2b820d1d1baa0c65bb5d1c1fb9b9e
Amaré Ngobeni <n.ngobeni@icloud.com>
2026-01-15 23:38:54 +0200

    create recursive factorial function

--- 08.functions/factorial/main.go
+++ 08.functions/factorial/main.go
@@ -3,5 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println(factorial(5))
+}
+
+func factorial(number int) int {
+	if number > 0 {
+		return number * factorial(number - 1)
+	}
+	return 1
 }
