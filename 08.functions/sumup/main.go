0f46190b2936ba4ab644aa00916d50b168b111c9
Amaré Ngobeni <n.ngobeni@icloud.com>
2026-01-16 22:23:56 +0200

    spread operator

--- 08.functions/sumup/main.go
+++ 08.functions/sumup/main.go
@@ -10,9 +10,11 @@ func main() {
 	numbers := []int{1, 2, 3}
 	sum1 := sumArray(&numbers)
 	sum2 := sum(2.4, 3, 4)
+	sum3 := sum(numbers...)
 
 	fmt.Println(sum1)
 	fmt.Println(sum2)
+	fmt.Println(sum3)
 }
 
 func sum[T Number](numbers ...T) T {
