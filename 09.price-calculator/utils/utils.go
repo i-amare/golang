1096aa8630fc8f39b3caf01e2fdc93b4f1064005
Amaré Ngobeni <n.ngobeni@icloud.com>
2026-01-17 19:59:46 +0200

    read prices from file

--- 09.price-calculator/utils/utils.go
+++ 09.price-calculator/utils/utils.go
@@ -1,9 +1,26 @@
 package utils
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
 func ReadPrice() []float32 {
 	prices := make([]float32, 0, 50)
 
-	// Add file logic
+	file, err := os.Open("store/prices.txt")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		fmt.Println(line)
+	}
 
 	return prices
 }
