d027ce652407bc6df89d47729909faff28ed8681
Amaré Ngobeni <n.ngobeni@icloud.com>
2026-01-17 22:12:20 +0200

    refactor

--- 09.price-calculator/main.go
+++ 09.price-calculator/main.go
@@ -7,34 +7,30 @@ import (
 	"example.com/price-calculator/utils"
 )
 
-var output = make([][]float64, 5)
-
 func main() {
 	taxRates := []int{0, 10, 14, 20, 24}
 	prices := utils.ReadPrice()
 
-	for _, val := range prices {
-		calcPostTaxPrices(val, &taxRates)
+	output := make([][]float64, 5)
+	for i := range output {
+		output[i] = make([]float64, 0, len(prices))
 	}
 
-	// utils.OutputPrices(taxRates, output)
-	for i, line := range output {
+	for _, val := range prices {
+		postTaxPrices := make([]float64, 0, len(taxRates))
+
+		for idx, taxRate := range taxRates {
+			newPrice := val * (1 + (float64(taxRate) / 100))
+			newPrice = math.Round(newPrice*100) / 100
+
+			output[idx] = append(output[idx], newPrice)
+			postTaxPrices = append(postTaxPrices, newPrice)
+		}
+	}
+
+	for i, val := range output {
 		fmt.Println("Tax Rate: ", taxRates[i])
-		fmt.Println(line)
+		fmt.Println(val)
 		fmt.Println("")
 	}
-}
-
-func calcPostTaxPrices(val float64, taxRates *[]int) []float64 {
-	postTaxPrices := make([]float64, 0, len(*taxRates))
-
-	for idx, taxRate := range *taxRates {
-		newPrice := val * (1 + (float64(taxRate) / 100))
-		newPrice = math.Round(newPrice*100) / 100
-
-		output[idx] = append(output[idx], newPrice)
-		postTaxPrices = append(postTaxPrices, newPrice)
-	}
-	
-	return postTaxPrices
 }
