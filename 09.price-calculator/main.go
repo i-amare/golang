781ae5f73a93cd21af64240d114d3ddba57f9165
Amaré Ngobeni <n.ngobeni@icloud.com>
2026-01-17 20:18:49 +0200

    calculate post tax prices

--- 09.price-calculator/main.go
+++ 09.price-calculator/main.go
@@ -7,6 +7,20 @@ import (
 )
 
 func main() {
+	taxRates := []int{0, 10, 14, 20, 24}
 	prices := utils.ReadPrice()
+
+	for _, val := range(prices) {
+		postTaxPrices := make([]float64, 0, len(taxRates))
+
+		for _, taxRate := range(taxRates) {
+			newPrice := val * (1 + (float64(taxRate)/100))
+			postTaxPrices = append(postTaxPrices, newPrice)
+		}
+		
+		fmt.Println(postTaxPrices)
+		fmt.Println("")
+	}
+
 	fmt.Println(prices)
 }
