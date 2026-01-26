7eee755be88147fa1747c072d76dec7cd8210814
Amaré Ngobeni <n.ngobeni@icloud.com>
2026-01-26 11:14:15 +0200

    using channels

--- 10.concurrency/starting-project/main.go
+++ 10.concurrency/starting-project/main.go
@@ -9,14 +9,17 @@ func greet(phrase string) {
 	fmt.Println("Hello!", phrase)
 }
 
-func slowGreet(phrase string) {
+func slowGreet(phrase string, done chan bool) {
 	time.Sleep(3 * time.Second) // simulate a slow, long-taking task
 	fmt.Println("Hello!", phrase)
+	done <- true
 }
 
 func main() {
 	greet("Nice to meet you!")
 	greet("How are you?")
-	slowGreet("How ... are ... you ...?")
+	done := make(chan bool)	
+	go slowGreet("How ... are ... you ...?", done)
 	greet("I hope you're liking the course!")
+	<- done
 }
