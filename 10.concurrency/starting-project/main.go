030cb029597eb242e07b1dc8677b577cac170019
Amaré Ngobeni <n.ngobeni@icloud.com>
2026-01-28 23:18:04 +0200

    close chan

--- 10.concurrency/starting-project/main.go
+++ 10.concurrency/starting-project/main.go
@@ -7,23 +7,25 @@ import (
 
 func greet(phrase string, done chan bool) {
 	fmt.Println("Hello!", phrase)
+	done <- true
 }
 
 func slowGreet(phrase string, done chan bool) {
 	time.Sleep(3 * time.Second) // simulate a slow, long-taking task
 	fmt.Println("Hello!", phrase)
 	done <- true
+	close(done)
 }
 
 func main() {
 	done := make(chan bool)
 
-	greet("Nice to meet you!", done)
-	greet("How are you?", done)
+	go greet("Nice to meet you!", done)
+	go greet("How are you?", done)
 	go slowGreet("How ... are ... you ...?", done)
-	greet("I hope you're liking the course!", done)
+	go greet("I hope you're liking the course!", done)
 
 	for range done {
-		fmt.Println("done")
+		// fmt.Println("done")
 	}
 }
