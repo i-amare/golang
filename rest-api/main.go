bd1211510a308056e8d42a2a9e3d394e91bb40a4
Amaré Ngobeni <n.ngobeni@icloud.com>
2026-02-07 22:23:54 +0200

    vendor GET

--- rest-api/main.go
+++ rest-api/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"net/http"
 
+	"github.com/i-amare/rest-api/models"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -10,10 +12,16 @@ func main() {
 	server := gin.Default()
 
 	server.GET("ping", ping)
+	server.GET("vendors", getVendors)
 
 	server.Run(":8080")
 }
 
 func ping(context *gin.Context) {
 	context.JSON(http.StatusOK, map[string]any{"res": 200, "msg": "hello world"})
+}
+
+func getVendors(context *gin.Context) {
+	v := models.GetVendors()
+	context.JSON(http.StatusOK, v)
 }
