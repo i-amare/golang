2627b14d9601740e40aa73ba78c6bbddb6868eca
Amaré Ngobeni <n.ngobeni@icloud.com>
2026-02-27 23:38:06 +0200

    create users func

--- rest-api/models/users.go
+++ rest-api/models/users.go
@@ -1,7 +1,34 @@
 package models
 
+import (
+	"fmt"
+
+	"github.com/i-amare/rest-api/db"
+)
+
 type User struct {
-	ID string
-	Email string
+	ID       int64
+	Email    string
 	Password string
+}
+
+func (u *User) Save() error {
+	query := `
+	INSERT INTO Users (Email, Password)
+	VALUES (?, ?)
+	`
+
+	res, err := db.DB.Exec(query, u.Email, u.Password)
+	if err != nil {
+		return err
+	}
+
+	id, err := res.LastInsertId()
+	u.ID = id
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("User saved successfully")
+	return nil
 }
--- rest-api/routes/users.go
+++ rest-api/routes/users.go
@@ -8,7 +8,26 @@ import (
 )
 
 func createUser(context *gin.Context) {
+	u, err := parseUserData(context)
+	if err != nil {
+		return
+	}
 
+	err = u.Save()
+	if err != nil {
+		res := gin.H{
+			"message": err.Error(),
+			"error":   err,
+		}
+		context.JSON(http.StatusInternalServerError, res)
+		return
+	}
+
+	res := gin.H{
+		"message": "User created",
+		"vendor":  u,
+	}
+	context.JSON(http.StatusCreated, res)
 }
 
 func parseUserData(context *gin.Context) (models.User, error) {
