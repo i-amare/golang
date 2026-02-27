1be585708f6f22fed606d76e344a01fe5b86d142
Amaré Ngobeni <n.ngobeni@icloud.com>
2026-02-27 23:22:30 +0200

    create user parsing func

--- rest-api/routes/users.go
+++ rest-api/routes/users.go
@@ -1,1 +1,27 @@
 package routes
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/i-amare/rest-api/models"
+)
+
+func createUser(context *gin.Context) {
+
+}
+
+func parseUserData(context *gin.Context) (models.User, error) {
+	var u models.User
+	err := context.ShouldBindJSON(&u)
+	if err != nil {
+		res := gin.H{
+			"message": "Error parsing data",
+			"data":    u,
+		}
+		context.JSON(http.StatusBadRequest, res)
+		return u, err
+	}
+
+	return u, nil
+}
