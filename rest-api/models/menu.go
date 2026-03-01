25874720594b19c0daaf8c45cb7b068abb50a320
Amaré Ngobeni <n.ngobeni@icloud.com>
2026-03-01 17:38:19 +0200

    saving item to db

--- rest-api/models/menu.go
+++ rest-api/models/menu.go
@@ -1,8 +1,40 @@
 package models
 
+import (
+	"fmt"
+
+	"github.com/i-amare/rest-api/db"
+)
+
 type MenuItem struct {
 	ID       int64
 	Name     string
 	Price    float64
 	VendorID int64
+}
+
+func (m MenuItem) Save() error {
+	query := `
+	INSERT INTO MenuItems (Name, Price, VendorID)
+	VALUES (?, ?, ?)
+	`
+
+	res, err := db.DB.Exec(query, m.Name, m.Price, m.VendorID)
+	if err != nil {
+		return err
+	}
+
+	id, err := res.LastInsertId()
+	m.ID = id
+	if err != nil {
+		return err
+	}
+
+	_, err = GetVendor(m.VendorID)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Menu item saved successfully")
+	return nil
 }
--- rest-api/routes/menu.go
+++ rest-api/routes/menu.go
@@ -7,8 +7,27 @@ import (
 	"github.com/i-amare/rest-api/models"
 )
 
-func createMenuItem() {
+func createMenuItem(context *gin.Context) {
+	m, err := parseMenuData(context)
+	if err != nil {
+		return
+	}
 
+	err = m.Save()
+	if err != nil {
+		res := gin.H{
+			"message": err.Error(),
+			"error":   err,
+		}
+		context.JSON(http.StatusInternalServerError, res)
+		return
+	}
+
+	res := gin.H{
+		"message":  "Menu item created",
+		"menuItem": m,
+	}
+	context.JSON(http.StatusOK, res)
 }
 
 func parseMenuData(context *gin.Context) (models.MenuItem, error) {
