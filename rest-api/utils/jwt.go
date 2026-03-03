6dd0ba7b91e39085f2e733f8295578cfaa5d6a09
Amaré Ngobeni <n.ngobeni@icloud.com>
2026-03-03 13:50:23 +0200

    create jwt token

---
+++ rest-api/utils/jwt.go
@@ -0,0 +1,19 @@
+package utils
+
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+const secretKey = "$2a$14$fP5BuSxkQP/nFAaLtEXRwuwy5a.TR7X7mtb/BjrorLLOaznFB/oPO"
+
+func GenerateAuthToken(email string, userID int64) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"email":  email,
+		"userID": userID,
+		"exp":    time.Now().Add(time.Hour * 24 * 7).Unix(),
+	})
+
+	return token.SignedString(secretKey)
+}
