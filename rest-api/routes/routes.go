6de3ba47a4a54082b240b66bfbcee6c50d5bfa75
Amaré Ngobeni <n.ngobeni@icloud.com>
2026-03-10 00:03:29 +0200

    run middleware

--- rest-api/routes/routes.go
+++ rest-api/routes/routes.go
@@ -10,16 +10,18 @@ func InitRoutes(server *gin.Engine) {
 	server.GET("/", ping)
 
 	server.GET("vendors", getAllVendors)
-	server.GET("vendors/:id", getVendor)
-	server.POST("vendors", createVendor)
-	server.PUT("vendors/:id", updateVendor)
-	server.DELETE("vendors/:id", deleteVendor)
-
+	server.GET("vendors/:id", getVendor)	
+	
 	server.POST("signup", createUser)
 	server.POST("login", loginUser)
 	server.GET("users", getAllUsers)
 
-	server.POST("menu", createMenuItem)
+	privileged := server.Group("/")
+	privileged.POST("vendors", createVendor)
+	privileged.PUT("vendors/:id", updateVendor)
+	privileged.DELETE("vendors/:id", deleteVendor)
+
+	privileged.POST("menu", createMenuItem)
 }
 
 func ping(context *gin.Context) {
