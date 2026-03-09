80d6b1740ca033638c4c801ac0a57b7eb0ec3f72
Amaré Ngobeni <n.ngobeni@icloud.com>
2026-03-09 23:53:02 +0200

    create auth middleware

---
+++ rest-api/middleware/auth.go
@@ -0,0 +1,32 @@
+package middleware
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/i-amare/rest-api/utils"
+)
+
+func Authenticate(context *gin.Context) {
+	token := context.Request.Header.Get("Authorization")
+
+	if token == "" {
+		res := gin.H{
+			"message": "No auth token provided",
+		}
+		context.AbortWithStatusJSON(http.StatusUnauthorized, res)
+		return
+	}
+
+	_, err := utils.VerifyAuthToken(token)
+	if err != nil {
+		res := gin.H{
+			"message": "Not authorised",
+			"error":   err.Error(),
+		}
+		context.AbortWithStatusJSON(http.StatusUnauthorized, res)
+		return
+	}
+
+	context.Next()
+}
